bd795adcc946e686ca725b35810ffc48fece25dd
peter-miles <103599491@student.swin.edu.au>
2022-03-30 12:59:55 +1100

    Inital project files commit

---
+++ Api/Controllers/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    public class PlayerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lib/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lib
+{
+    public class Player
+    {
+        public int PlayerID { get; set; }
+        public string PlayerName { get; set; }
+        public int PlayerMinutes { get; set; }
+        public int PlayerPlusMinus {get; set; }
+        // public string PlayerTeam { get; set; }
+    }
+}
