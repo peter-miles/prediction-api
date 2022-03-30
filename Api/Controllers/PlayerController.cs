fc7323e051b985593616db75fc7a888163f80f87
peter-miles <80442779+peter-miles@users.noreply.github.com>
2022-03-30 14:33:15 +1100

    Added GetAllPlayers to PlayerController
    Needs to be tested, for use with front end

--- Api/Controllers/PlayerController.cs
+++ Api/Controllers/PlayerController.cs
@@ -1,16 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Data;
+using System.Data.SqlClient;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+using Lib;
 
 namespace Api.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class PlayerController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        public List<Player> GetAllPlayers()
         {
-            return View();
+            List<Player> Players = new List<Player>();
+
+            using (SqlConnection sqlConnection = new SqlConnection("workstation id=nbaprediction.mssql.somee.com;packet size=4096;user id=teamsouth_SQLLogin_1;pwd=daouqkr2hp;data source=nbaprediction.mssql.somee.com;persist security info=False;initial catalog=nbaprediction"))
+            {
+                SqlCommand sqlCommand = new SqlCommand("SELECT * FROM PLAYERS", sqlConnection);
+
+                sqlConnection.Open();
+                using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                {
+                    while (sqlDataReader.Read())
+                    {
+                        Player TempPlayer = new Player((int)sqlDataReader[0], sqlDataReader[1].ToString(), sqlDataReader[2].ToString(), (float)sqlDataReader[3], (float)sqlDataReader[4]);
+                        Players.Add(TempPlayer);
+                    }
+                }
+            }
+
+            return Players;
         }
     }
 }
